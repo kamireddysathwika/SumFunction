6cb307e18189f4c42cee932a63275539e8d8855a
kamireddysathwika <kamireddysathwika0908@gmail.com>
2025-01-30 15:23:06 +0530

    Add files via upload

---
+++ SumFunction.java
@@ -0,0 +1,11 @@
+public class SumFunction {
+ public static void main(String[] args) {
+        int num1 = 15;
+        int num2 = 25;
+        int result = sum(num1, num2);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
